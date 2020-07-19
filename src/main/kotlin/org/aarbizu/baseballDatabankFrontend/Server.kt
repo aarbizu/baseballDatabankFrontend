969b46e7629a2748e4cab8e03bc82bc10dc5239d
Alan Arbizu <alan.arbizu@gmail.com>
2020-07-19 14:28:46 -0700

    refactor the kweb route receiver to keep the websocket calls working

--- src/main/kotlin/org/aarbizu/baseballDatabankFrontend/Server.kt
+++ src/main/kotlin/org/aarbizu/baseballDatabankFrontend/Server.kt
@@ -13,43 +13,61 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.websocket.WebSockets
 import java.time.Duration
+import kweb.ElementCreator
 import kweb.Kweb
 import kweb.button
-import kweb.h1
-import kweb.input
+import kweb.li
+import kweb.new
+import kweb.plugins.fomanticUI.fomantic
+import kweb.plugins.fomanticUI.fomanticUIPlugin
 import kweb.respondKwebRender
-import kweb.span
-import kweb.state.KVar
+import kweb.route
+import kweb.toInt
+import kweb.ul
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(10)
+        pingPeriod = Duration.ofSeconds(15)
         timeout = Duration.ofSeconds(30)
     }
     install(Kweb) {
+        plugins = listOf(fomanticUIPlugin)
         routing {
-            get("/") {
-                call.respondKwebRender { _ ->
-                    val greeting = browser.url
-                        .map { it.removePrefix("/") }
-                        .map { "Hello, " + if (it.isNotBlank()) it else "World" }
-
-                    val next = KVar("")
-                    h1().text(greeting)
-                    span().text("Where to next?")
-                    input().setValue(next)
-                    button().text("Go!").on.click {
-                        browser.url.value = next.value
-                    }
+            get("/{visitedUrl...}") {
+                call.respondKwebRender {
+                    getRouteReceiver()
                 }
             }
         }
     }
 }
+
+private fun ElementCreator<*>.getRouteReceiver() {
+    route {
+        path("/") {
+            browser.url.value = "/number/1"
+        }
+        path("/number/{num}") { params ->
+            var ulist = ul(fomantic.ui.bulleted.list).new {
+                li(fomantic.ui.bulleted).text("num = ${ params.getValue("num").value }")
+            }
+            val num = params.getValue("num").toInt()
+            button(fomantic.ui.button).text(num.map { "Number $it" }).on.click {
+                num.value++
+                ulist.innerHTML("num = ${ params.getValue("num").value }")
+            }
+        }
+    }
+}
+
 class Server {
     fun start() {
-        embeddedServer(Netty, 8080, watchPaths = listOf("ServerKt"), module = Application::module).start()
+        embeddedServer(Netty,
+                8080,
+                watchPaths = listOf("baseballDatabankFrontend"),
+                module = Application::module
+        ).start()
     }
 }
