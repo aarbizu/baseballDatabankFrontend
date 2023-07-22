196f278bf7337bad464f7227c9dea8b117ebfedc
Alan Arbizu <alan.arbizu@gmail.com>
2023-07-21 22:41:55 -0700

    Minor tidy-up
    * remove unneeded parens in NoPlayersFound.kt
    
    * remove StatsMenuProps which were made obsolete after switching to
      redux for state mgmt

--- src/jsMain/kotlin/org/aarbizu/baseballDatabankFrontend/StatsMenu.kt
+++ src/jsMain/kotlin/org/aarbizu/baseballDatabankFrontend/StatsMenu.kt
@@ -8,15 +8,10 @@ import react.router.useNavigate
 import react.useState
 import web.dom.Element
 
-external interface StatsMenuProps : BasicMenuProps {
-    var offenseStatLabels: List<String>
-    var pitchingStatLabels: List<String>
-}
-
 @Serializable data class StatNames(var hitting: List<String>, var pitching: List<String>)
 
 val StatsMenu =
-    FC<StatsMenuProps> { props ->
+    FC<BasicMenuProps> { props ->
         val (menuAnchorElem, menuAnchorSetter) = useState<Element>()
         val navigate = useNavigate()
 
